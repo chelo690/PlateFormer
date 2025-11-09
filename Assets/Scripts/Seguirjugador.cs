0fce52c4a9d61ee3f32058a194d50329c64699d5
ghostman5226 <samiqt2004@gmail.com>
2025-11-09 14:20:29 -0500

    Código Olvido, seguir jugador por area

---
+++ Assets/Scripts/Seguirjugador.cs
@@ -0,0 +1,127 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Seguirjugador : MonoBehaviour
+{
+    public float radioBusqueda;
+
+    public LayerMask capaJugador;
+
+    public Transform transformJugador;
+
+    public float velocidadMovimiento;
+
+    public float distanciaMaxima;
+
+    public Vector3 puntoInicial;
+
+    public bool mirandoDerecha;
+
+    public EstadosMovimiento estadoActual;
+
+    public enum EstadosMovimiento
+    {
+        Esperando,
+
+        Siguiendo,
+
+        Volviendo,
+
+    }
+
+    private void Start()
+    {
+        puntoInicial = transform.position;
+    }
+
+    private void Update()
+    {
+        switch (estadoActual)
+        {
+            case EstadosMovimiento.Esperando:
+                EstadoEsperando();
+                break;
+            case EstadosMovimiento.Siguiendo:
+                EstadoSiguiendo();
+                break;
+            case EstadosMovimiento.Volviendo:
+                EstadoVolviendo();
+                break;   
+        }
+
+        
+    }
+
+    private void EstadoEsperando()
+    {
+        Collider2D jugadorCollider = Physics2D.OverlapCircle(transform.position, radioBusqueda, capaJugador);
+
+        if (jugadorCollider)
+        {
+            transformJugador = jugadorCollider.transform;
+
+            estadoActual = EstadosMovimiento.Siguiendo;
+        }
+    }
+
+    private void EstadoSiguiendo()
+    {
+        if (transformJugador == null)
+        {
+            estadoActual = EstadosMovimiento.Volviendo;
+            return;
+        }
+
+        transform.position = Vector2.MoveTowards(transform.position, transformJugador.position, velocidadMovimiento * Time.deltaTime);
+
+        GirarAObjetivo(transformJugador.position);
+
+        if (Vector2.Distance(transform.position, puntoInicial) > distanciaMaxima ||
+            Vector2.Distance(transform.position, transformJugador.position) > distanciaMaxima)
+        {
+            estadoActual = EstadosMovimiento.Volviendo;
+            transformJugador = null;
+        }
+    }
+
+    private void EstadoVolviendo()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, puntoInicial, velocidadMovimiento * Time.deltaTime);
+
+        GirarAObjetivo(puntoInicial);
+
+        if (Vector2.Distance(transform.position, puntoInicial) < 0.1f)
+        {
+            estadoActual = EstadosMovimiento.Esperando;
+        }
+    }
+
+    private void GirarAObjetivo(Vector3 objetivo)
+    {
+        if (objetivo.x > transform.position.x && !mirandoDerecha)
+        {
+            Girar();
+        }
+        else if (objetivo.x < transform.position.x && mirandoDerecha)
+        {
+            Girar();
+        }
+    }
+
+    private void Girar()
+    {
+        mirandoDerecha = !mirandoDerecha;
+        transform.eulerAngles = new Vector3(0, transform.eulerAngles.y + 180, 0);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, radioBusqueda);
+        Gizmos.DrawWireSphere(puntoInicial, distanciaMaxima);
+    }
+
+
+}
